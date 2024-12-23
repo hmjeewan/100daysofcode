929f6de3a990346aa8c01c621001cb6907a98470
[Jeewan Nandasena] <[hmjeewan@gmail.com]>
2024-12-23 19:43:56 +0530

    first commit

---
+++ Backend/Python/Day01.py
@@ -0,0 +1,8 @@
+#Single line comment
+#Day 1 - Hello World
+
+#Multi line comment
+'''1. Write a program that prints "Hello World!" to the console
+2. Add single line and multi-line comments to your code'''
+
+print("Hello World!")
