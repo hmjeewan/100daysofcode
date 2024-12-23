c94f8aff3bf76153b2e5060252e81c8c3ba07fbf
[Jeewan Nandasena] <[hmjeewan@gmail.com]>
2024-12-23 20:19:47 +0530

    Adding java file

---
+++ Backend/Java/src/Day01.java
@@ -0,0 +1,12 @@
+//Day 1 - Hello World
+/*
+1. Write a program that prints "Hello World!" to the console
+2. Add single line and multi-line comments to your code
+*/
+
+public class Day01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
