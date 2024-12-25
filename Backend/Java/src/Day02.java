c928bb3494ae53c56d9fd9260b38d476e75a14a8
[Jeewan Nandasena] <[hmjeewan@gmail.com]>
2024-12-25 22:35:23 +0530

    Day 02

---
+++ Backend/Java/src/Day02.java
@@ -0,0 +1,12 @@
+//Day 2 - Variables and Data Types
+/*1. Declare and initialize variables for various data types including integer (int), floating-point number (double), and string (String). Then, print each variable to the console.*/
+
+public class Day02 {
+    public static void main(String[] args) {
+        int age=39;
+        double height=5.2;
+        String name="jeewan";
+        System.out.println("My name is "+ name +", my age is "+ age+ " & i'm "+ height+ "foots tall");
+
+    }
+}
---
+++ Backend/Python/Day02.py
@@ -0,0 +1,25 @@
+#Day 2 - Variables
+'''Create the following variables of different types and print them.
+name: String
+age: Integer
+height: Floating-point number
+is_student: Boolean
+email: None (representing a null value)
+siblings: List
+address: Dictionary containing street and city'''
+
+name = "jeewan"
+age=39
+height = 5.6
+is_student = True
+email = "hmjeewan@gmail.com"
+siblings = [0]
+address={
+    'no':32,
+    'street':'Mahindarama Road',
+    'city':'Badulla'
+}
+
+print("My name is ", name," my age is ",age," my height is",height, " my email is ", email)
+print("My siblings", siblings)
+print("My address is", address)
