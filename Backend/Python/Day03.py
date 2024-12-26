5da83d9fb71e489ee14b32dcb43cdf688bb23c7c
[Jeewan Nandasena] <[hmjeewan@gmail.com]>
2024-12-26 19:05:59 +0530

    Day 01

---
+++ Backend/Python/Day03.py
@@ -0,0 +1,10 @@
+#Day 3 - Input and Output
+'''Basic Input and Output:
+Write a program that reads a single input from the user and prints it to the console. For example, if the user enters their name, the program should output: ""Hello, {name}""
+'''
+name = input("Enter your name: ")
+print("Hello, " + name)
+age=int(input("Enter your age: "))
+print("Your age is"+str(age))
+height=float(input("Enter your height: "))
+print("Your height is"+str(height))
