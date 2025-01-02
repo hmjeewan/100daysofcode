3800f4c14005aa6b1b2d20a5ba3df7fe76ecb3fe
[Jeewan Nandasena] <[hmjeewan@gmail.com]>
2025-01-02 22:21:09 +0530

    Day07

---
+++ Backend/Java/src/Day07.java
@@ -0,0 +1,19 @@
+//Day 7 - Math Operations
+/*Write a program that performs the following tasks using Math class:
+Maximum of 2 numbers
+Minimum of 2 numbers
+Square root of a number
+Floor and Ceil of a float
+Abs value of a double
+
+ */
+public class Day07 {
+    public static void main(String[] args) {
+        System.out.println(Math.max(2,5));
+        System.out.println(Math.min(2,5));
+        System.out.println(Math.sqrt(23));
+        System.out.println(Math.ceil(2.3));
+        System.out.println(Math.floor(2.3));
+        System.out.println(Math.abs(2.3));
+    }
+}
---
+++ Backend/Python/Day07.py
@@ -0,0 +1,10 @@
+#Day 7 - Temparature Conversion
+#1. Write a program to convert temperature from Celsius to Fahrenheit.
+cel=float(input("Enter Celsius: "))
+far=(cel*9/5)+32
+print("Fahrenheit value is",far)
+
+#2. Write a program to convert temperature from Fahrenheit to Celsius
+faren=float(input("Enter Fahrenheit: "))
+celcius=(faren-32)*5/9
+print("Celsius value is",celcius)
