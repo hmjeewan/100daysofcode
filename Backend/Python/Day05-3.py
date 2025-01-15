3ce37d9d7950a2cd931fcb10a8d8b4b3e77dd092
Jeewan <40348826+hmjeewan@users.noreply.github.com>
2025-01-15 19:06:20 +0530

    Update Day05-3.py
    Found the bug & fix it

--- Backend/Python/Day05-3.py
+++ Backend/Python/Day05-3.py
@@ -2,7 +2,7 @@
 '''3. Write a program that checks if a given input year is a leap year or not'''
 
 year = int(input('Enter the year: '))
-if year % 4 == 0 and year % 100 != 0 :
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print('The year is a leap year')
 else:
     print('The year is not a leap year')
